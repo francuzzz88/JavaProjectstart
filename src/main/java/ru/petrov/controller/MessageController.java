54cd82b8a3373b3a0b807eef21a480089f3ddef4
francuzzz88 <_francuzzz_@mail.ru>
2020-01-25 16:35:22 +0300

    Add Open Graph Protocol

--- src/main/java/ru/petrov/controller/MessageController.java
+++ src/main/java/ru/petrov/controller/MessageController.java
@@ -2,25 +2,40 @@ package ru.petrov.controller;
 
 
 import com.fasterxml.jackson.annotation.JsonView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.petrov.domain.Message;
 import ru.petrov.domain.Views;
 import ru.petrov.dto.EventType;
+import ru.petrov.dto.MetaDto;
 import ru.petrov.dto.ObjectType;
 import ru.petrov.repository.MessageRepository;
 import ru.petrov.util.WsSender;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-private final MessageRepository messageRepository;
-private final BiConsumer<EventType, Message> wsSender;
+    private static String URL_PATTERN = "https?:\\/\\/?[\\w\\d\\._\\-%\\/\\?=&#]+";
+    private static String IMAGE_PATTERN = "\\.(jpeg|jpg|gif|png)$";
+
+    private static Pattern URL_REGEX = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static Pattern IMG_REGEX = Pattern.compile(IMAGE_PATTERN, Pattern.CASE_INSENSITIVE);
+
+
+    private final MessageRepository messageRepository;
+    private final BiConsumer<EventType, Message> wsSender;
 
     @Autowired
     public MessageController(MessageRepository messageRepository, WsSender wsSender) {
@@ -30,39 +45,78 @@ private final BiConsumer<EventType, Message> wsSender;
 
     @GetMapping
     @JsonView(Views.IdName.class)
-    public List<Message> list(){
+    public List<Message> list() {
         return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
     @JsonView(Views.FullMessage.class)
-    public Message getOne(@PathVariable("id") Message message){
+    public Message getOne(@PathVariable("id") Message message) {
         return message;
     }
 
 
-
     @PostMapping
-    public Message create(@RequestBody Message message){
+    public Message create(@RequestBody Message message) throws IOException {
         message.setTimeCreation(LocalDateTime.now());
+        fillMeta(message);
         Message updateMessage = messageRepository.save(message);
         wsSender.accept(EventType.CREATE, updateMessage);
         return updateMessage;
     }
 
     @PutMapping("{id}")
-    public Message update(@PathVariable("id") Message messageFromDb, @RequestBody Message message){
+    public Message update(@PathVariable("id") Message messageFromDb, @RequestBody Message message) throws IOException {
         BeanUtils.copyProperties(message, messageFromDb, "id");
+        fillMeta(messageFromDb);
         Message updateMessage = messageRepository.save(messageFromDb);
         wsSender.accept(EventType.UPDATE, updateMessage);
         return updateMessage;
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable("id") Message message){
+    public void delete(@PathVariable("id") Message message) {
         messageRepository.delete(message);
         wsSender.accept(EventType.REMOVE, message);
     }
 
 
+    private void fillMeta(Message message) throws IOException {
+        String text = message.getText();
+        Matcher matcher = URL_REGEX.matcher(text);
+
+        if (matcher.find()) {
+            String url = text.substring(matcher.start(), matcher.end());
+            matcher = IMG_REGEX.matcher(url);
+
+            message.setLink(url);
+
+            if (matcher.find()) {
+                message.setLinkCover(url);
+            } else if (url.contains("youtu")) {
+                MetaDto meta = getMeta(url);
+
+
+                message.setLinkCover(meta.getCover());
+                message.setLinkTitle(meta.getTitle());
+                message.setLinkDescription(meta.getDescription());
+            }
+        }
+    }
+
+    private MetaDto getMeta(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        Elements title = document.select("meta[name$=title], meta[property$=title]");
+        Elements description = document.select("meta[name$=description], meta[property$=description]");
+        Elements cover = document.select("meta[name$=image], meta[property$=image]");
+        return new MetaDto(
+                getContent(title.first()),
+                getContent(description.first()),
+                getContent(cover.first())
+        );
+    }
+
+    private String getContent(Element element) {
+        return element == null? "" : element.attr("content");
+    }
 }
--- src/main/java/ru/petrov/domain/Message.java
+++ src/main/java/ru/petrov/domain/Message.java
@@ -28,4 +28,12 @@ public class Message {
     @JsonView(Views.FullMessage.class)
     private LocalDateTime timeCreation;
 
+    @JsonView({Views.FullMessage.class})
+    private String link;
+    @JsonView({Views.FullMessage.class})
+    private String linkTitle;
+    @JsonView({Views.FullMessage.class})
+    private String linkDescription;
+    @JsonView({Views.FullMessage.class})
+    private String linkCover;
    }
